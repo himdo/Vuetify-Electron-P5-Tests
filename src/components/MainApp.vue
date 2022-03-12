aa23e6349d2b84e23d8bf03f6f10a6fc314ac376
lennon <hitzemanl@yahoo.com>
2022-03-11 16:13:23 -0800

    Added a lot more elements to the main app; added color picker; added list of led modes; added apply / reset buttons

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -3,38 +3,79 @@
 
     <v-row class="text-center">
       <v-col
-        class="mb-5"
-        cols="12">
-        <v-sheet>
-        <div class="d-flex justify-content-center" id="p5Canvas"></div>
+        class="mb-5">
+        <v-container width="this.innerWidth">
+          <div class="d-flex" style="justify-content: center;" id="p5Canvas"></div>
 
-        </v-sheet>
+        </v-container>
       </v-col>
-
+      <v-divider/>
       <v-col
         class="mb-5"
         cols="12"
       >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
+        <v-container>
+          <v-row no-gutters class="mb-6">
+            <v-col
+                sm="8">
+              <v-card
+                class="pa-2"
+                outlined
+                tile
+              >
+                <v-container>
+                  <v-row no-gutters>
+                    <v-col>
+                      <v-color-picker
+                        dot-size="25"
+                        swatches-max-height="200"
+                      ></v-color-picker>
+                    </v-col>
+                    <v-col>
+                      <h5>LED Mode:</h5>
+                      <v-divider></v-divider>
+                      <v-container id="scroll-target"
+                        style="max-height: 200px"
+                        class="overflow-y-auto"
+                      >
+                        <v-list v-scroll>
+                          <v-list-item-group v-model="model">
+                            <v-list-item
+                              v-for="(item, i) in ledModes"
+                              :key="i"
+                            >
+                              <v-list-item-content>
+                                <v-list-item-title v-text="item.text"></v-list-item-title>
+                              </v-list-item-content>
+                            </v-list-item>
+                          </v-list-item-group>
+                        </v-list>
+                      </v-container>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card>
+            </v-col>
+            <v-col
+                sm="4">
+              <v-card
+                class="pa-2"
+                outlined
+                tile
+              >
+                <v-row>
+                  <v-col>
+                    <v-btn>Reset</v-btn>
+                  </v-col>
+                  <v-col>
+                    <v-btn>Apply</v-btn>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
-
-      
-
-      
     </v-row>
   </v-container>
 </template>
@@ -45,67 +86,74 @@
     name: 'HelloWorld',
 
     data: () => ({
-      ecosystem: [
+      innerWidth: window.innerWidth,
+      spawnedP5: false,
+      ledModes: [
         {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
+          text: 'Static',
         },
         {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
+          text: 'Rainbow Wave',
         },
         {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
+          text: 'Crosshair',
         },
         {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
+          text: 'Reactive Fade',
         },
         {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
+          text: 'Custom',
         },
         {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
+          text: 'Stars',
         },
         {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
+          text: 'Snowing',
         },
         {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+          text: 'Color Cycle',
         },
         {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+          text: 'Breathing',
+        },
+        {
+          text: 'Reactive Punch',
+        },
+        {
+          text: 'Circle Spectrum',
+        },
+        {
+          text: 'Reactive Tornado',
+        },
+        {
+          text: 'Water Ripple',
+        },
+        {
+          text: 'Turn Off',
         },
       ],
     }),
+    watch: {
+
+    },
     mounted() {
-      const P5 = require('p5')
-      new P5(keyboardRender.main)
-      // // NOTE: p5.jsからのコールバックを受け取る
-      // radar.setDelegate(this.callbackOnP5);
+      this.$nextTick(() => {
+        window.addEventListener('resize', this.onResize);
+      })
+
+      if (this.spawnedP5 === false) {
+        const P5 = require('p5')
+        new P5(keyboardRender.main)
+        this.spawnedP5 = true
+      }
+    },
+    beforeDestroy() { 
+      window.removeEventListener('resize', this.onResize); 
     },
     methods: {
-      // callbackOnP5: function(timeStr) {
-      //   this.message = timeStr;
-      // }
+      onResize() {
+        this.innerWidth = window.innerHeight
+      }
     }
   }
 
